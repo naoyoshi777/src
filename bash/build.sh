88d32ee1d281030a10f55e62530e959988d96203
Naoyoshi Murakami <n-murakami@satake-japan.co.jp>
2020-12-11 13:49:11 +0900

    build.sh: add

---
+++ bash/build.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+#
+# Generic build script
+
+set -u
+
+readonly LOGFILE='buildlog.txt'
+
+SECONDS=0
+
+LANG=C make "$@" 2>&1 | tee "${LOGFILE}"
+
+ret=${PIPESTATUS[0]}
+
+duration=${SECONDS}
+
+if [ ${ret} -eq 0 ]; then
+  echo -e '\e[1;32m'
+  cat <<EOF
+        @@@@@@@@@@          @@@@@@    @@@@@@@@
+    @@@@          @@@@        @@        @@
+    @@              @@        @@      @@
+  @@                  @@      @@    @@
+  @@                  @@      @@  @@
+  @@                  @@      @@@@  @@
+  @@                  @@      @@      @@
+    @@              @@        @@        @@
+    @@@@          @@@@        @@          @@
+        @@@@@@@@@@          @@@@@@          @@@@
+EOF
+  echo -e '\e[m'
+else
+  echo -e '\e[1;31m'
+  cat <<EOF
+  @@@@          @@@@@@@@          @@@@@@@@@@  @@
+    @@@@            @@        @@@@          @@@@
+    @@ @@           @@        @@              @@
+    @@   @@         @@      @@
+    @@     @@       @@      @@
+    @@       @@     @@      @@          @@@@@@@@@@
+    @@         @@   @@      @@                @@
+    @@           @@ @@        @@              @@
+    @@            @@@@        @@@@          @@@@
+  @@@@@@@@          @@            @@@@@@@@@@
+EOF
+  echo -e '\e[m'
+
+  grep -A2 'error:\|FAILED' "${LOGFILE}"
+fi
+
+hr=
+
+if (( duration >= 3600 )); then
+  hr=$(( duration / 3600 ))
+  duration=$(( duration % 3600 ))
+fi
+
+min=
+
+if (( duration >= 60 )); then
+  min=$(( duration / 60 ))
+  duration=$(( duration % 60 ))
+fi
+
+sec=${duration}
+
+echo "Time: ${hr:+${hr}h }${min:+${min}m }${sec}s"
