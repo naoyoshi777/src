c79cf2ab2f381fd9d53db915820fb67f89f6fe77
Naoyoshi Murakami <naoyoshi777@gmail.com>
2021-02-21 20:43:44 +0900

    bash: install-gnu-global: exit script if error

--- bash/install-gnu-global.sh
+++ bash/install-gnu-global.sh
@@ -5,8 +5,8 @@
 readonly VERSION='6.6.5'
 
 sudo dpkg -r global 2> /dev/null
-sudo apt -y install libncurses5-dev exuberant-ctags python-pip checkinstall
-sudo -H pip install pygments "${http_proxy:+--proxy ${http_proxy}}"
+sudo apt -y install libncurses5-dev exuberant-ctags python-pip checkinstall || exit $?
+sudo -H pip install pygments "${http_proxy:+--proxy ${http_proxy}}" || exit $?
 
 wget http://tamacom.com/global/global-${VERSION}.tar.gz || exit $?
 tar xf global-${VERSION}.tar.gz
