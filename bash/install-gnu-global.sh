7a941ba7e2e795cb089b303d230d9bfd80dcf1bd
Naoyoshi Murakami <n-murakami@satake-japan.co.jp>
2021-02-24 12:47:12 +0900

    bash: install-gnu-global.sh: remove files on exit

--- bash/install-gnu-global.sh
+++ bash/install-gnu-global.sh
@@ -4,9 +4,18 @@
 
 readonly VERSION='6.6.5'
 
+clean_up() {
+  cd && rm -rf "${tmpdir}"
+}
+
 sudo dpkg -P global 2> /dev/null
 sudo apt -y install libncurses5-dev exuberant-ctags python-pip checkinstall || exit $?
 sudo -H pip install pygments ${http_proxy:+--proxy ${http_proxy}} || exit $?
+
+trap 'clean_up' EXIT
+
+tmpdir="$(mktemp -d)"
+cd "${tmpdir}"
 
 wget http://tamacom.com/global/global-${VERSION}.tar.gz || exit $?
 tar xf global-${VERSION}.tar.gz
@@ -17,4 +26,6 @@ make
 sudo checkinstall --nodoc -y
 
 cp -a /usr/local/share/gtags/gtags.conf ~/.globalrc
-echo 'export GTAGSLABEL=pygments' >> ~/.bashrc
+echo 'export GTAGSLABEL=pygments' >> ~/.profile
+
+echo -e '\e[1;32mSucceeded to install GNU Global.\e[m'
