f343a8f762bc1a3bdb32c4ac77c3c3ec2730e7a2
Naoyoshi Murakami <n-murakami@satake-japan.co.jp>
2021-02-24 09:18:08 +0900

    bash: add install-ricty.sh

---
+++ bash/install-ricty.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+#
+# Script to install Ricty font
+
+clean_up() {
+  cd && rm -rf "${tmpdir}"
+}
+
+if type fontforge &> /dev/null; then
+  sudo apt install fontforge || exit $?
+fi
+
+trap 'clean_up' EXIT
+
+tmpdir="$(mktemp -d)"
+cd "${tmpdir}"
+
+wget https://rictyfonts.github.io/files/ricty_generator.sh || exit $?
+chmod +x ricty_generator.sh
+
+wget https://osdn.net/projects/mix-mplus-ipa/downloads/72511/migu-1m-20200307.zip || exit $?
+unzip migu-1m-20200307.zip
+mv migu-1m-20200307/migu-1m-* .
+
+git clone https://github.com/googlefonts/Inconsolata.git || exit $?
+
+./ricty_generator.sh auto
+
+mkdir -p ~/.local/share/fonts/
+mv Ricty-*.ttf ~/.local/share/fonts/
+fc-cache -fv
+
+echo -e '\e[1;32mSucceeded to install Ricty font.\e[m'
